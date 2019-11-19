994fd58a37f7f9e6c86ab1efbfafea7cd050d67b
Sanctuscoma <konink1992@gmail.com>
2019-11-20 00:01:42 +0100

    Added src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import Head from '../../components/head'
+import Nav from '../../components/nav'
 
 const Home = () => (
   <div>
